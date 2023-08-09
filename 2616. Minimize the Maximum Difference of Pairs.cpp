b86a525adf01785517ed55893fda687521e8718d
Aryan Gupta <aryang23936@gmail.com>
2023-08-09 12:11:35 +0530

    09-08-2023

---
+++ 2616. Minimize the Maximum Difference of Pairs.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    bool good(int x, vector<int> &nums, int p) {
+        int count = 0, n = nums.size();
+        for(int i=0; i<n-1; i++) {
+            if(nums[i+1] - nums[i] <= x){
+                count++;
+                i++;
+            }
+        }
+        return count >= p;
+    }
+public:
+    int minimizeMax(vector<int>& nums, int p) {
+        sort(nums.begin(), nums.end());
+        int s = 0, e = 1e9, ans;
+        while(s <= e) {
+            int m = (s + e) / 2;
+            if(good(m, nums, p)) e = m - 1, ans = m;
+            else s = m + 1;
+        }
+        return ans;
+    }
+};
